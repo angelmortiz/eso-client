a30df5dc9dc5a56f868efd287579b7ce028e3c45
Angel Ortiz <angelmanuelortiz@gmail.com>
2022-10-03 16:40:57 -0400

    Creating the navigation bar part 1

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,9 +1,13 @@
+import styles from './NavigationBar.module.css';
+
 function NavigationBar() {
     return <nav>
-        <a href="/" className="site-logo"><img alt="En Optima Salud official logo"></img></a>
-        <ul>
-            <li>Nutrition</li>
-            <li>Activities</li>
+        <a href="/" className="logo-link">
+            <img src= "logo.png" className={styles['logo-img']} alt="En Optima Salud official logo"></img>
+        </a>
+        <ul className={styles['ul']}>
+            <a href="#"><li className={styles['li']}>Nutrition</li></a>
+            <a href="#"><li className={styles['li']}>Activities</li></a>
         </ul>
     </nav>
 }
