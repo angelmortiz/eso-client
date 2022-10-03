480daba145a87af24c6ea8013ecc446b8e2b1111
Angel Ortiz <angelmanuelortiz@gmail.com>
2022-10-03 13:42:16 -0400

    Minor fixes

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,6 @@
 function NavigationBar() {
     return <nav>
-        <a href="/" className="site-logo"><img></img></a>
+        <a href="/" className="site-logo"><img alt="En Optima Salud official logo"></img></a>
         <ul>
             <li>Nutrition</li>
             <li>Activities</li>
