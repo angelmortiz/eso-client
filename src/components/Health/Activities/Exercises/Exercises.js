08a634b2eaa3c3c52cd6cbc37b7f9a320d51f5eb
Angel Ortiz <angelmanuelortiz@gmail.com>
2022-10-27 14:55:07 -0400

    Connecting Exercises page to DB

--- src/components/Health/Activities/Exercises/Exercises.js
+++ src/components/Health/Activities/Exercises/Exercises.js
@@ -1,44 +1,31 @@
 import InfoCard from "../../General/Cards/InfoCard";
 import classes from "./Exercises.module.css";
 import { fetchAllExercises } from "../../../../util/apis/exercises/exercisesApis"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Exercises = props => {
     const [exercises, setExercises] = useState([]);
 
-    if (!exercises || exercises.length ===  0) {
+    useEffect(() => {
         fetchAllExercises().then(data => { 
-            console.log("Exercises data: ", data);
+            console.log("data: ", data);
             setExercises(data);
         });
-    }
+    }, []);
+
+    const addInfoCards = () => {
+        let infoCards = [];
+        infoCards = exercises.map(exercise => {
+            return <InfoCard key={exercise._id} info={exercise} />
+        });
+        return infoCards;
+    };
 
     return <div className={classes['grid']}>
-        <h1 className={classes['title']}>Exercises</h1>
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
-        <InfoCard id="630a1b779f9e9bd37eb241c5" />
+        <h1 className={classes['title']}>Exercises</h1>        
+        { exercises && exercises.length 
+                ? addInfoCards()
+                : <img src="/loading.gif" alt="Loading..." className={classes['loading-img']}/>}
     </div>
 };
 
--- src/components/Health/General/Cards/InfoCard.js
+++ src/components/Health/General/Cards/InfoCard.js
@@ -1,29 +1,29 @@
-import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import classes from "./InfoCard.module.css"
 
 const InfoCard = props => {
+    const info = props.info;
 
-    return <Fragment>
-        <div className={classes['card']}> 
-            <img src="/squat.jpg" alt="Squat" className={classes['img']}/>
+    return <div className={classes['card']}> 
+            <img src={info.linkToImage} alt="Squat" className={classes['img']}/>
             <div className={classes['exercise-info']}>
                 <div className={classes['names']}>
-                    <h1 className={classes['name']}>Squats</h1>
-                    <h2 className={classes['alternative-name']}>Sentadillas</h2>
+                    <h1 className={classes['name']}>{info.name}</h1>
+                    <h2 className={classes['alternative-name']}>{info.alternativeName}</h2>
                 </div>
                 <div className={classes['general-info']}>
                     <div className={classes['info-block']}>
                         <p className={classes['label']}>Muscle:</p>
-                        <p className={classes['value']}>Quadriceps</p>
+                        <p className={classes['value']}>{info.mainMuscle.muscleName}</p>
                     </div>
                     <div className={classes['info-block']}>
                         <p className={classes['label']}>Equipment:</p>
-                        <p className={classes['value']}>Barbell</p>
+                        {/* TODO: Handle scenarios where there is more than one equipment */}
+                        <p className={classes['value']}>{info.equipments[0]?.equipmentName}</p>
                     </div>
                     <div className={classes['info-block']}>
                         <p className={classes['label']}>Compound:</p>
-                        <p className={classes['value']}>Yes</p>
+                        <p className={classes['value']}>{info.compoundMovement ? 'Yes' : 'No'}</p>
                     </div>
                 </div>
             </div>
@@ -31,7 +31,6 @@ const InfoCard = props => {
                 Details
             </Link>
         </div>
-    </Fragment>
 };
 
 export default InfoCard;
