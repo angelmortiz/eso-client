30bf7da822ba79c44cec1dfd936b79010300ab83
Angel Ortiz <angelmanuelortiz@gmail.com>
2023-06-09 16:57:45 -0400

    Updating assigned-program grid

--- src/components/Health/Activities/ProgramPlans/AssignedProgramPlans.js
+++ src/components/Health/Activities/ProgramPlans/AssignedProgramPlans.js
@@ -1,92 +1,37 @@
-import { useEffect, useState } from 'react';
-import { fetchProgramPlansAssignedToUser } from '../../../../util/apis/activities/programPlans/programPlansApis';
-import styles from '../../../UI/General/CSS/ProgramPlan.module.css';
-import ProgramPlanInfoCard from './ProgramPlanInfoCard';
+import { useEffect, useState } from "react";
+import { fetchProgramPlansAssignedToUser } from "../../../../util/apis/activities/programPlans/programPlansApis";
+import ProgramPlanInfoCard from "./ProgramPlanInfoCard";
+import ProgramsGridView from "../../../UI/Grids/ProgramsGridView";
 
 const AssignedProgramPlans = (props) => {
   const [assignedProgramPlans, setAssignedProgramPlans] = useState();
-  const [activeProgramPlans, setActiveProgramPlans] = useState();
-  const [nextProgramPlans, setNextProgramPlans] = useState();
-  const [pendingProgramPlans, setPendingProgramPlans] = useState();
 
   useEffect(() => {
     fetchProgramPlansAssignedToUser().then((response) => {
       if (!response || !response.isSuccess) return;
+
+      //TODO: add property to assigned programs to flag the active ones and next
       //console.log('Response: ', response);
       setAssignedProgramPlans(response.body);
-
-      //active programPlan
-      setActiveProgramPlans(
-        response.body.filter((programPlan) => programPlan.logs?.log?.isStarted)
-      );
-      //next programPlan
-      setNextProgramPlans(
-        response.body.filter(
-          (programPlan) => !programPlan.logs?.log?.isStarted
-        )[0]
-      );
-      //pending programPlans
-      let pending = response.body.filter(
-        (programPlan) => !programPlan.logs?.log?.isStarted
-      );
-      if (pending.length > 0) {
-        pending.shift();
-      }
-      setPendingProgramPlans(pending);
     });
   }, []);
 
-  return (
-    <div className={styles['grid']}>
-      <h1 className={styles['page-title']}>Assigned Plans</h1>
-
-      {/* LOADING  IMAGE... */}
-      {!assignedProgramPlans && (
-        <img
-          src="/loading.gif"
-          alt="Loading..."
-          className={styles['loading-img']}
+  const addInfoCards = () => {
+    let infoCards = [];
+    infoCards = assignedProgramPlans?.map((program) => {
+      return (
+        <ProgramPlanInfoCard
+          programPlan={program}
+          activateStart={true}
+          key={`program-plan_next_${program._id}`}
         />
-      )}
+      );
+    });
+    return infoCards;
+  };
 
-      {/* ACTIVE PLANS */}
-      {activeProgramPlans && (
-        <section id="active" className={styles['programPlan-section']}>
-          <h3 className={styles['section-label']}>Active Plans</h3>
-          {activeProgramPlans.map((programPlan, index) => (
-            <ProgramPlanInfoCard
-              programPlan={programPlan}
-              key={`program-plan_active_${index}`}
-            />
-          ))}
-        </section>
-      )}
-
-      {/* NEXT PLANS */}
-      {nextProgramPlans && (
-        <section id="next" className={styles['programPlan-section']}>
-          <h3 className={styles['section-label']}>Next Plan</h3>
-          <ProgramPlanInfoCard
-            programPlan={nextProgramPlans}
-            activateStart={true}
-            key={`program-plan_next`}
-          />
-        </section>
-      )}
-
-      {/* PENDING PLANS */}
-      {pendingProgramPlans && (
-        <section id="pending" className={styles['programPlan-section']}>
-          <h3 className={styles['section-label']}>Pending Plans</h3>
-          {pendingProgramPlans.map((programPlan, index) => (
-            <ProgramPlanInfoCard
-              programPlan={programPlan}
-              key={`program-plan_pending_${index}`}
-            />
-          ))}
-        </section>
-      )}
-    </div>
+  return (
+    <ProgramsGridView title="Assigned Plans">{addInfoCards()}</ProgramsGridView>
   );
 };
 
---
+++ src/components/UI/Grids/ProgramsGridView.js
@@ -0,0 +1,24 @@
+const ProgramsGridView = (props) => {
+  const { title, children } = props;
+
+  return (
+    <div className="my-6 flex flex-col px-2 sm:my-8 sm:px-8">
+      <h1 className="mx-auto mb-5 text-2xl font-semibold leading-6 text-gray-900 ">
+        {title}
+      </h1>
+      {!children ? (
+        <img
+          src="/loading.gif"
+          alt="Loading..."
+          className="mx-auto h-10 w-10"
+        />
+      ) : (
+        <ul className="grid grid-cols-1 gap-2 lg:grid-cols-2 2xl:grid-cols-3">
+          {children.length > 0 && children}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ProgramsGridView;
