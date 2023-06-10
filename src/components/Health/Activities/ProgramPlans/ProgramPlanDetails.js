1035ae88ef93a57742574a9dcb543a18c57ebac3
Angel Ortiz <angelmanuelortiz@gmail.com>
2023-06-10 14:51:27 -0400

    Part2 ProgramPlanDetails Update

--- src/components/Health/Activities/ProgramPlans/ProgramPlanDetails.js
+++ src/components/Health/Activities/ProgramPlans/ProgramPlanDetails.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { fetchProgramPlanById } from "../../../../util/apis/activities/programPlans/programPlansApis";
 
@@ -11,7 +11,6 @@ const ProgramPlanDetails = (props) => {
     fetchProgramPlanById(id).then((response) => {
       if (!response || !response.isSuccess) return;
 
-      console.log("Response: ", response);
       setProgramPlan(response.body);
     });
   }, [id]);
@@ -22,7 +21,7 @@ const ProgramPlanDetails = (props) => {
         <img
           src="/loading.gif"
           alt="Loading..."
-          className="mt-10 mx-auto h-10 w-10"
+          className="mx-auto mt-10 h-10 w-10"
         />
       ) : (
         <div className="mx-5 mt-10 overflow-hidden rounded-lg bg-white px-4 py-6 shadow lg:mx-auto lg:max-w-[75%]">
@@ -74,12 +73,6 @@ const ProgramPlanDetails = (props) => {
                     </th>
                     <th
                       scope="col"
-                      className="hidden px-3 py-2 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-                    >
-                      Variant
-                    </th>
-                    <th
-                      scope="col"
                       className="hidden px-3 py-2 text-left text-sm font-semibold text-gray-900 md:table-cell"
                     >
                       Type
@@ -93,56 +86,66 @@ const ProgramPlanDetails = (props) => {
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {programPlan.program?.workouts?.map((workout, index) => (
-                    <tr key={`workout_${index}`}>
-                      <td className="w-1/3 max-w-0 py-4 pl-3 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
-                        {programPlan.program.sequence === "Weekly"
-                          ? workout.dayOfTheWeek
-                          : workout.dayNumber}
-                      </td>
-                      <td className="px-3 py-4 text-sm text-gray-800">
-                        <Link
-                          to={`/activities/workout/${workout.workout?._id}`}
-                          className="underline underline-offset-4"
-                        >
-                          {workout.workout?.name}
-                        </Link>
-                        <dl className="font-normal lg:hidden">
-                          <dt className="sr-only">Variant</dt>
-                          <dd className="mt-1 truncate text-gray-500">
-                            <Link
-                              to={`/activities/workout/${workout.workout?._id}`}
-                              className="underline underline-offset-4"
-                            >
-                              {workout.workout.variant}
-                            </Link>
-                          </dd>
-                          <dt className="sr-only lg:hidden">Type</dt>
-                          <dd className="mt-1 truncate text-gray-500 md:hidden">
-                            {workout.workout.type}
-                          </dd>
-                          <dt className="sr-only sm:hidden">Target</dt>
-                          <dd className="mt-1 truncate text-gray-500 sm:hidden">
-                            {workout.workout.target}
-                          </dd>
-                        </dl>
-                      </td>
-                      <td className="hidden px-3 py-4 text-sm text-gray-500  underline underline-offset-4 lg:table-cell">
-                        <Link
-                          to={`/activities/workout/${workout.workout?._id}`}
-                          className="underline underline-offset-4"
-                        >
-                          {workout.workout.variant}
-                        </Link>
-                      </td>
-                      <td className="hidden px-3 py-4 text-sm text-gray-500 md:table-cell">
-                        {workout.workout.type}
-                      </td>
-                      <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                        {workout.workout.target}
-                      </td>
-                    </tr>
-                  ))}
+                  {/* Weeks Breakdown */}
+                  {programPlan?.weeksPlan &&
+                    programPlan?.weeksPlan.map((weekPlan) => (
+                      <Fragment key={`week_header_${weekPlan.weekNumber}`}>
+                        <tr className="border-t border-gray-200">
+                          <th
+                            colSpan={5}
+                            scope="colgroup"
+                            className="bg-gray-100 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                          >
+                            Week {weekPlan.weekNumber}
+                          </th>
+                        </tr>
+                        {/* Program Plan Breakdown */}
+                        {weekPlan.workouts?.map(
+                          (workout, index) => (
+                            <tr key={`workout_${index}`}>
+                              <td className="w-1/3 max-w-0 py-4 pl-3 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
+                                {programPlan.program.sequence === "Weekly"
+                                  ? workout.dayOfTheWeek
+                                  : workout.dayNumber}
+                              </td>
+                              <td className="px-3 py-4 text-sm text-gray-800">
+                                <Link
+                                  to={`/activities/workout/${workout.workout?._id}`}
+                                  className="underline underline-offset-4"
+                                >
+                                  {workout.workout?.name}
+                                </Link>
+                                <dl className="font-normal lg:hidden">
+                                  <dt className="sr-only">Variant</dt>
+                                  <dd className="mt-1 truncate text-gray-500">
+                                    <Link
+                                      to={`/activities/workout/${workout.workout?._id}`}
+                                      className="underline underline-offset-4"
+                                    >
+                                      {workout.workout.variant}
+                                    </Link>
+                                  </dd>
+                                  <dt className="sr-only lg:hidden">Type</dt>
+                                  <dd className="mt-1 truncate text-gray-500 md:hidden">
+                                    {workout.workout.type}
+                                  </dd>
+                                  <dt className="sr-only sm:hidden">Target</dt>
+                                  <dd className="mt-1 truncate text-gray-500 sm:hidden">
+                                    {workout.workout.target}
+                                  </dd>
+                                </dl>
+                              </td>
+                              <td className="hidden px-3 py-4 text-sm text-gray-500 md:table-cell">
+                                {workout.workout.type}
+                              </td>
+                              <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                                {workout.workout.target}
+                              </td>
+                            </tr>
+                          )
+                        )}
+                      </Fragment>
+                    ))}
                 </tbody>
               </table>
             </div>
