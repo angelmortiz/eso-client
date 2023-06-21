51f662e4b757b9797896a9499cac679797271a65
Angel Ortiz <angelmanuelortiz@gmail.com>
2023-06-21 12:27:12 -0400

    PART 4 Renaming .js files to .jsx

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice.jsx';
-import userReducer from './userSlice.js';
-import toastReducer from './toastNotificationSlice.js';
+import authReducer from './authSlice';
+import userReducer from './userSlice';
+import toastReducer from './toastNotificationSlice';
 
 const store = configureStore({
   reducer: { auth: authReducer, userInfo: userReducer, toast: toastReducer },
