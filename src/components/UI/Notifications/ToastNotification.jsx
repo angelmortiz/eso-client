7ddc8d3f93b0494b6b5bc22fcd4cea08610b9ad4
Angel Ortiz <angelmanuelortiz@gmail.com>
2023-06-21 12:51:31 -0400

    Updating packages

--- src/components/UI/Notifications/ToastNotification.jsx
+++ src/components/UI/Notifications/ToastNotification.jsx
@@ -7,7 +7,7 @@ import {
   ExclamationTriangleIcon,
   ExclamationCircleIcon,
 } from "@heroicons/react/24/outline";
-import { XMarkIcon } from "@heroicons/react/20/solid";
+//import { XMarkIcon } from "@heroicons/react/20/solid";
 
 const types = {
   success: "success",
