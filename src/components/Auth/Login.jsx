78cb5d889e685a523fb6326367534087af1ca9d1
Angel Ortiz <angelmanuelortiz@gmail.com>
2023-06-23 14:31:32 -0400

    Minor fixes to Login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -42,7 +42,7 @@ const Login = (props) => {
   const loginUser = (e) => {
     e.preventDefault();
 
-    if (!isValidationSuccessful()) return;
+    if (!isValid()) return;
 
     login(formValues).then((response) => {
       if (!response || !response.isSuccess) {
@@ -51,7 +51,7 @@ const Login = (props) => {
       }
 
       dispatch(authActions.login());
-      navigateTo("/activities/exercises");
+      navigateTo("/");
       getCurrentUserInfo();
     });
   };
@@ -64,7 +64,7 @@ const Login = (props) => {
     });
   };
 
-  const isValidationSuccessful = () => {
+  const isValid = () => {
     const { email, password } = formValues;
     let errors = {
       email: [],
@@ -127,12 +127,12 @@ const Login = (props) => {
 
             <div className="flex items-center justify-between">
               <div className="text-sm">
-                <Link
-                  to="/auth/forgotPassword"
+                <a
+                  href="/auth/forgotPassword"
                   className="font-medium text-cyan-700 hover:text-cyan-600"
                 >
                   Forgot your password?
-                </Link>
+                </a>
               </div>
             </div>
 
@@ -159,8 +159,11 @@ const Login = (props) => {
             </div>
 
             <div className="mt-6 flex flex-col gap-4">
-              <Link
-                to=""
+              <a
+                href={
+                  process.env.REACT_APP_API_URL +
+                  process.env.REACT_APP_AUTH_GOOGLE
+                }
                 className="flex w-full items-center justify-center gap-3 rounded-md bg-[#DB4437] px-3 py-2 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#DB4437]"
               >
                 <svg
@@ -174,9 +177,12 @@ const Login = (props) => {
                 <span className="text-sm font-semibold leading-6">
                   Continue with Google
                 </span>
-              </Link>
-              <Link
-                to=""
+              </a>
+              <a
+                href={
+                  process.env.REACT_APP_API_URL +
+                  process.env.REACT_APP_AUTH_FACEBOOK
+                }
                 className="flex w-full items-center justify-center gap-3 rounded-md bg-[#3b5998] px-3 py-2 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#3b5998]"
               >
                 <svg
@@ -189,7 +195,7 @@ const Login = (props) => {
                 <span className="text-sm font-semibold leading-6">
                   Continue with Facebook
                 </span>
-              </Link>
+              </a>
               <Link
                 to="/auth/signup"
                 className="flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-2 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
@@ -201,7 +207,6 @@ const Login = (props) => {
                   viewBox="0 0 512 512"
                 >
                   <path
-                    class="st0"
                     d="M510.678,112.275c-2.308-11.626-7.463-22.265-14.662-31.054c-1.518-1.915-3.104-3.63-4.823-5.345 c-12.755-12.818-30.657-20.814-50.214-20.814H71.021c-19.557,0-37.395,7.996-50.21,20.814c-1.715,1.715-3.301,3.43-4.823,5.345 C8.785,90.009,3.63,100.649,1.386,112.275C0.464,116.762,0,121.399,0,126.087V385.92c0,9.968,2.114,19.55,5.884,28.203 c3.497,8.26,8.653,15.734,14.926,22.001c1.59,1.586,3.169,3.044,4.892,4.494c12.286,10.175,28.145,16.32,45.319,16.32h369.958 c17.18,0,33.108-6.145,45.323-16.384c1.718-1.386,3.305-2.844,4.891-4.43c6.27-6.267,11.425-13.741,14.994-22.001v-0.064 c3.769-8.653,5.812-18.171,5.812-28.138V126.087C512,121.399,511.543,116.762,510.678,112.275z M46.509,101.571 c6.345-6.338,14.866-10.175,24.512-10.175h369.958c9.646,0,18.242,3.837,24.512,10.175c1.122,1.129,2.179,2.387,3.112,3.637 L274.696,274.203c-5.348,4.687-11.954,7.002-18.696,7.002c-6.674,0-13.276-2.315-18.695-7.002L43.472,105.136 C44.33,103.886,45.387,102.7,46.509,101.571z M36.334,385.92V142.735L176.658,265.15L36.405,387.435 C36.334,386.971,36.334,386.449,36.334,385.92z M440.979,420.597H71.021c-6.281,0-12.158-1.651-17.174-4.552l147.978-128.959 l13.815,12.018c11.561,10.046,26.028,15.134,40.36,15.134c14.406,0,28.872-5.088,40.432-15.134l13.808-12.018l147.92,128.959 C453.137,418.946,447.26,420.597,440.979,420.597z M475.666,385.92c0,0.529,0,1.051-0.068,1.515L335.346,265.221L475.666,142.8 V385.92z"
                   />
                 </svg>
